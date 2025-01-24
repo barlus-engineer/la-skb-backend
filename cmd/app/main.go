6fbcf02d926f7d9dc39f769ff5822d26767a440e
Barlus Cuda <barluscuda@gmail.com>
2025-01-24 18:18:05 +0700

    add whutdown logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"la-skb/Internal/app/database"
 	"la-skb/Internal/routers"
 	"la-skb/config"
@@ -20,5 +24,28 @@ func main() {
 
 	Addr := fmt.Sprintf("%s:%s", cfg.IP, cfg.Port)
 	logger.Info(fmt.Sprintf("Server is started on http://%s", Addr))
+
+	// Signal handling for graceful shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Print("\r")
+		logger.Info("Shutting down server....")
+		os.Exit(0)
+	}()
+
+	// Goroutine to listen for "stop" command
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			input, _ := reader.ReadString('\n')
+			if input == "stop\n" {
+				logger.Info("Received stop command. Shutting down server....")
+				os.Exit(0)
+			}
+		}
+	}()
+
 	serve.Run(Addr)
 }
